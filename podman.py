eaf04ed0e276149a2ed086a83e104192e123a821
Bradley A. Thornton <bthornto@redhat.com>
2024-02-21 08:20:27 -0800

    spelling fix

--- podman.py
+++ podman.py
@@ -13,11 +13,11 @@ except ValueError:
     pass
 
 if ARGS[1] in SUPPORTED:
-    print(f"Setting CONTIANER_HOST to use kubedock")
-    os.environ["CONTIANER_HOST"] = "tcp://127.0.0.1:2475"
+    print(f"Setting CONTAINER_HOST to use kubedock")
+    os.environ["CONTAINER_HOST"] = "tcp://127.0.0.1:2475"
 else:
-    print(f"Unsetting CONTIANER_HOST to use podman locally")
-    os.environ.pop("CONTIANER_HOST", None)
+    print(f"Unsetting CONTAINER_HOST to use podman locally")
+    os.environ.pop("CONTAINER_HOST", None)
 
 ARGS[0] = "podman.orig"
 
