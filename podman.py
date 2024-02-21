a1529d979b4e528030f3e172f3e5bbaa1408c351
Bradley A. Thornton <bthornto@redhat.com>
2024-02-21 08:03:42 -0800

    print

--- podman.py
+++ podman.py
@@ -13,10 +13,13 @@ except ValueError:
     pass
 
 if ARGS[1] in SUPPORTED:
+    print(f"Setting CONTIANER_HOST to use kubedock")
     os.environ["CONTIANER_HOST"] = "tcp://127.0.0.1:2475"
 else:
+    print(f"Unsetting CONTIANER_HOST to use podman locally")
     os.environ.pop("CONTIANER_HOST", None)
 
 ARGS[0] = "podman.orig"
 
+print(f"Executing: {' '.join(ARGS)}")
 os.execvp(ARGS[0], ARGS)
