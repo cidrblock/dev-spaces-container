e3debf80285e6a9e6059f8e3b42c895828bf6e28
Bradley A. Thornton <bthornto@redhat.com>
2024-02-21 07:47:56 -0800

    Select podman commands

---
+++ an.sh
@@ -0,0 +1,1 @@
+ansible-navigator run site.yml --ll debug --la false --pae false --mode stdout --eei ghcr.io/cidrblock/cidrblock-ee:latest
--- podman.py
+++ podman.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 ARGS = sys.argv
+SUPPORTED = ("run", "ps", "exec", "cp", "logs", "inspect", "kill", "rm", "wait", "stop", "start")
 
 try:
     ARGS.remove("--interactive")
@@ -11,6 +12,10 @@ try:
 except ValueError:
     pass
 
+if ARGS[1] in SUPPORTED:
+    ARGS.insert(2,"tcp://127.0.0.1:2475")
+    ARGS.insert(2,"--connection")
+
 ARGS[0] = "podman.orig"
-    
+  
 os.execvp(ARGS[0], ARGS)
